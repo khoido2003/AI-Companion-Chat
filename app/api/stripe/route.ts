0caee61fb1df1b44d4ebb7a4d45534a6402e541f
Khoi Do <khoizpro31@gmail.com>
2024-02-03 22:56:19 +0700

    Add Stripe API

---
+++ app/api/stripe/route.ts
@@ -0,0 +1,65 @@
+import prismadb from "@/lib/prismadb";
+import { stripe } from "@/lib/stripe";
+import { absoluteUrl } from "@/lib/utils";
+import { auth, currentUser } from "@clerk/nextjs";
+import { NextResponse } from "next/server";
+
+const settingUrl = absoluteUrl("/settings");
+
+export async function GET() {
+  try {
+    const { userId } = auth();
+    const user = await currentUser();
+
+    if (!userId || !user) {
+      return new NextResponse("Unauthorized", { status: 401 });
+    }
+
+    const userSubscription = await prismadb.userSubscription.findUnique({
+      where: {
+        userId,
+      },
+    });
+
+    if (userSubscription && userSubscription.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: userSubscription.stripeCustomerId,
+        return_url: settingUrl,
+      });
+      return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+    }
+
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: settingUrl,
+      cancel_url: settingUrl,
+      payment_method_types: ["card"],
+      mode: "subscription",
+      billing_address_collection: "auto",
+      customer_email: user.emailAddresses[0].emailAddress,
+      line_items: [
+        {
+          price_data: {
+            currency: "USD",
+            product_data: {
+              name: "Premium",
+              description: "Create custom AI Companions",
+            },
+            unit_amount: 999,
+            recurring: {
+              interval: "month",
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId,
+      },
+    });
+
+    return new NextResponse(JSON.stringify({ url: stripeSession.url }));
+  } catch (err) {
+    console.log("STRIPE_ERROR", err);
+    return NextResponse.redirect("INTERNAL_ERROR", { status: 500 });
+  }
+}
